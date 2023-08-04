3b592892c0fe218a9ea6468fbe4655dea11c1ed1
syahrulrama0 <syahrulramadhanstelk@gmail.com>
2023-08-04 15:53:38 +0700

    split test case

--- Test Suites/Register/Register Success.ts
+++ Test Suites/Register/Register Success.ts
@@ -12,9 +12,33 @@
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>3a6170f5-9227-4178-8151-7bb8724ae2b7</testSuiteGuid>
    <testCaseLink>
+      <guid>8a5a7d9f-221f-4732-b0c6-50f3d2aeca30</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Open Browser</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5a7695dd-d2f8-45d1-85b7-169042e13452</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Navigate to Url</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
       <guid>ef025d8f-93b8-4707-876d-154c217b3f27</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Register</testCaseId>
+      <testCaseId>Test Cases/Fill Registerd Form</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ef9e7f67-d253-42e3-9e3e-e24aa17e00bb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Submit Registered Form</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>33f1f4f5-deed-42f1-b92a-4f0b8733185e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Close Browser</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
